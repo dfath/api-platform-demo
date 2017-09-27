0bbadc2db203059fc8d34b84cc31a58a5ac2e3af
dfath <fathrico@gmail.com>
2017-09-27 09:44:58 +0700

    add AcmeBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,6 +18,7 @@ class AppKernel extends Kernel
             new ApiPlatform\Core\Bridge\Symfony\Bundle\ApiPlatformBundle(),
             new Nelmio\CorsBundle\NelmioCorsBundle(),
             new AppBundle\AppBundle(),
+            new Suteki\Siakad\AcmeBundle\AcmeBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
